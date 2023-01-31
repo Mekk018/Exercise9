fcdbe95e679754a2f1fc99f057de1100b0d9a240
SIWAKORN SIRIPHAN <mekk.siriphan@gmail.com>
2023-01-31 13:24:41 +0700

    Input_while_or

---
+++ Exercise9.py
@@ -0,0 +1,8 @@
+usernameInput = input("USERNAME : ")
+passwordInput = input("PASSWORD : ")
+while usernameInput != "admin" or passwordInput != "1129" :
+    print("--------------------")
+    usernameInput = input("USERNAME : ")
+    passwordInput = input("PASSWORD : ")
+print("DONE")
+print("--------------------")
